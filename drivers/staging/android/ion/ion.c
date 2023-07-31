6c1784213b3dab10a5ec8d56cc81cc6c71e3c5e5
nantaaa <anantah738@gmail.com>
2023-07-31 23:41:21 +0700

    Revert "ion: Add compatibility with legacy Ion v1 ABI"
    This reverts commit b8801638466b0d70b21d1e972ff8244f1a59b159.

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -52,7 +52,6 @@
 
 #include "ion.h"
 #include "ion_secure_util.h"
-#include "compat_ion.h"
 
 static struct ion_device *internal_dev;
 
@@ -1248,11 +1247,7 @@ static const struct file_operations ion_fops = {
 	.owner          = THIS_MODULE,
 	.unlocked_ioctl = ion_ioctl,
 #ifdef CONFIG_COMPAT
-#ifdef CONFIG_ION_LEGACY
-	.compat_ioctl	= compat_ion_ioctl,
-#else
 	.compat_ioctl	= ion_ioctl,
-#endif
 #endif
 };
 
